68b541ee042e98a63d921a0c004c0d44688f8928
LRKZZ <81513533+neznayu-hub@users.noreply.github.com>
2023-05-11 22:35:31 +0600

    Add files via upload

---
+++ ImathInterface/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ImathInterface
+{
+    class Third : IMath
+    {
+        public double Add(double v1, double v2)
+        {
+            return v1 + v2;
+        }
+
+        public double Substract(double v1, double v2)
+        {
+            return v1 - v2;
+        }
+
+        public double Multiply(double v1, double v2)
+        {
+            return v1 * v2;
+        }
+
+        public double Divide(double v1, double v2)
+        {
+            if (v2 == 0)
+                throw new DivideByZeroException("На ноль делить нельзя");
+            return v1 / v2;
+        }
+    }
+}
